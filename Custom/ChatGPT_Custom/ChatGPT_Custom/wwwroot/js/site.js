b3f9ba75e783db0b90fedcae95f43594b00ddbe3
Nick Rosati <nickrosati2@mac.com>
2025-12-20 12:55:31 -0500

    Visuals

---
+++ Custom/ChatGPT_Custom/ChatGPT_Custom/wwwroot/js/site.js
@@ -0,0 +1,6 @@
+﻿function scrollToBottom() {
+    var chatMessages = document.getElementsByClassName("chat-messages")[0];
+    if (chatMessages) {
+        chatMessages.scrollTop = chatMessages.scrollHeight;
+    }
+}
